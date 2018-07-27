867e5fc49a957776e1f72a2eb578ce6a90cbd864
Guilherme GM <guilherme.moreira@beetech.global>
2018-07-27 16:30:48 -0300

    feat(jwt): add authentication function

--- src/common/jwt.js
+++ src/common/jwt.js
@@ -1,9 +1,28 @@
-var jsonwebtoken = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = process.env.GASTARME_JWT_SECRET || 'hard-secret'
 
+const authentication = ({ accessLevels = ['user', 'admin'], User }) => async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return next({ message: 'You are not authenticated' })
+  }
+
+  const token = req.headers.authorization.split(' ')[1]
+  const decoded = jwt.verify(token, JWT_SECRET)
+
+  const user = await User.findById(decoded.id)
+
+  if (!user || !accessLevels.includes(user.accessLevel)) {
+    return next({ message: 'You are not authorized' })
+  }
+
+  req.user = user
+
+  return next()
+}
+
 const sign = ({ id }) => {
-  return jsonwebtoken.sign(
+  return jwt.sign(
     {
       id,
     },
@@ -12,4 +31,4 @@ const sign = ({ id }) => {
   )
 }
 
-module.exports = { sign }
+module.exports = { authentication, sign }
