c2f44d6b3253fcd23561eaf590d9911fa930522f
Guilherme GM <guilherme.moreira@beetech.global>
2018-07-27 12:27:56 -0300

    feat(card): add migration to create cards table

---
+++ sequelize/migrations/20180727051009-create-card.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      number: {
+        type: Sequelize.STRING
+      },
+      bearer_name: {
+        type: Sequelize.STRING
+      },
+      cvv: {
+        type: Sequelize.STRING
+      },
+      validity: {
+        type: Sequelize.DATEONLY
+      },
+      limit: {
+        type: Sequelize.DECIMAL(12,2)
+      },
+      maturity: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Cards');
+  }
+};
