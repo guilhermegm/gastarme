32d17d7fb16f74eed2e0757d692c6bf279299e14
Guilherme GM <guilherme.moreira@beetech.global>
2018-07-30 01:34:17 -0300

    refactor(sequelize): update connection vars to be ready for production on common

--- src/common/sequelize.js
+++ src/common/sequelize.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('', '', '', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-})
+const sequelize = new Sequelize(
+  process.env.GASTARME_DB_DATABASE || 'gastarme',
+  process.env.GASTARME_DB_USERNAME || 'gastarme',
+  process.env.GASTARME_DB_PASSWORD || 'gastarme',
+  {
+    host: process.env.GASTARME_DB_HOST || 'postgres',
+    port: process.env.GASTARME_DB_PORT || 5432,
+    dialect: 'postgres',
+  },
+)
 
 const authenticate = () => sequelize.authenticate()
 
